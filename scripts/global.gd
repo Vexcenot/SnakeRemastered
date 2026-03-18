62c5ca47a5365bbc3deab584281c6b465c5f418f
Vexcenot <yoshimasacyz@gmail.com>
2026-03-18 22:45:11 +0800

    buffered maze spawning to new var

--- scripts/global.gd
+++ scripts/global.gd
@@ -1,16 +1,16 @@
 extends Node
 var sceneGame = preload("res://scenes scripts/game.tscn")
-var maze1 = preload("res://scenes scripts/maze1.tscn" )
-var maze2 = preload("res://scenes scripts/maze2.tscn" )
-var maze3 = preload("res://scenes scripts/maze3.tscn" )
-var maze4 = preload("res://scenes scripts/maze4.tscn" )
-var maze5 = preload("res://scenes scripts/maze5.tscn" )
-
+var maze1 = preload("res://scenes scripts/maze1.tscn")
+var maze2 = preload("res://scenes scripts/maze2.tscn")
+var maze3 = preload("res://scenes scripts/maze3.tscn")
+var maze4 = preload("res://scenes scripts/maze4.tscn")
+var maze5 = preload("res://scenes scripts/maze5.tscn")
+var levelSpawn = sceneGame
 
 
 
 var sceneMenu = preload("res://scenes scripts/menu.tscn")
-var ScenetoSpawn = sceneGame
+var ScenetoSpawn = levelSpawn
 
 var speed : int = 9#6
 var originalTime : float = 1 - (speed*0.1)
--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,25 +1,16 @@
 extends Control
 
+func _ready():
+	Global.ScenetoSpawn = Global.levelSpawn
+
 func _process(_delta: float) -> void:
 	if $ColorRect/Level.visible:
 		Global.updateTime()
 		$ColorRect/Level/VolBars.frame = Global.speed - 1
+		Global.ScenetoSpawn = Global.levelSpawn
 
 var level :bool = true
 
-func _on_new_game_button_down() -> void:
-	Global.changeScene.emit()
-	await get_tree().process_frame
-	queue_free()
-
-#make it spawn scene ontop of main scene
-func _on_level_button_down() -> void:
-	$ColorRect/Level.visible = true
-	$ColorRect/Menu.visible = false
-
-func _on_button_button_down() -> void:
-	$ColorRect/Level.visible = false
-	$ColorRect/Menu.visible = true
 
 func _input(event: InputEvent) -> void:
 	#key presses if level screen active
@@ -31,6 +22,8 @@ func _input(event: InputEvent) -> void:
 			if Global.speed <= 8:
 				Global.speed += 1
 
+
+#cancel back to menu
 	if Input.is_action_pressed("ui_cancel") or Input.is_action_pressed("ui_accept"):
 		if $ColorRect/Level.visible:
 			$ColorRect/Level.visible = false
@@ -43,61 +36,94 @@ func _input(event: InputEvent) -> void:
 			$ColorRect/Menu.visible = true
 
 
+
+########
+#LEVEL BUTTONS
+########
 func _on__button_down() -> void:
 	Global.speed = 1
 	$ColorRect/Level/VolBars.frame = 0
-
-
 
 func _on__button_down2() -> void:
 	Global.speed = 2
 	$ColorRect/Level/VolBars.frame = 1
 
-
 func _on__button_down3() -> void:
 	Global.speed = 3
 	$ColorRect/Level/VolBars.frame = 2
-
 
 func _on__button_down4() -> void:
 	Global.speed = 4
 	$ColorRect/Level/VolBars.frame = 3
 
-
 func _on__button_down5() -> void:
 	Global.speed = 5
 	$ColorRect/Level/VolBars.frame = 4
-
 
 func _on__button_down6() -> void:
 	Global.speed = 6
 	$ColorRect/Level/VolBars.frame = 5
 
-
 func _on__button_down7() -> void:
 	Global.speed = 7
 	$ColorRect/Level/VolBars.frame = 6
-
 
 func _on__button_down8() -> void:
 	Global.speed = 8
 	$ColorRect/Level/VolBars.frame = 7
 
-
 func _on__button_down9() -> void:
 	Global.speed = 9
 	$ColorRect/Level/VolBars.frame = 8
  
+func _on_button_button_down() -> void:
+	$ColorRect/Level.visible = false
+	$ColorRect/Menu.visible = true
 
 
+
+########
+#INSTRUCTIONS BUTTONS
+########
 func _on_ok_button_down2() -> void:
 	$ColorRect/Instructions.visible = false
 	$ColorRect/Menu.visible = true
 
 
-func _on_instructions_button_down() -> void:
-	$ColorRect/Instructions.visible = true
-	$ColorRect/Menu.visible = false
+
+
+
+########
+#MAZE MENU BUTTONS
+########
+func _on_no_maze_button_down() -> void:
+	Global.levelSpawn = Global.sceneGame
+	Global.ScenetoSpawn = Global.levelSpawn
+
+
+func _on_maze_1_button_down() -> void:
+	Global.levelSpawn = Global.maze1
+	Global.ScenetoSpawn = Global.levelSpawn
+
+
+func _on_maze_2_button_down() -> void:
+	Global.levelSpawn = Global.maze2
+	Global.ScenetoSpawn = Global.levelSpawn
+
+
+func _on_maze_3_button_down() -> void:
+	Global.levelSpawn = Global.maze3
+	Global.ScenetoSpawn = Global.levelSpawn
+
+
+func _on_maze_4_button_down() -> void:
+	Global.levelSpawn = Global.maze4
+	Global.ScenetoSpawn = Global.levelSpawn
+
+
+func _on_maze_5_button_down() -> void:
+	Global.levelSpawn = Global.maze5
+	Global.ScenetoSpawn = Global.levelSpawn
 
 
 func _on_ok_button_down3() -> void:
@@ -105,30 +131,25 @@ func _on_ok_button_down3() -> void:
 	$ColorRect/Menu.visible = true
 
 
-func _on_mazes_button_down() -> void:
-	$ColorRect/Mazes.visible = true
+
+
+########
+#MAIN MENU BUTTONS
+########
+func _on_new_button_down() -> void:
+	Global.changeScene.emit()
+	await get_tree().process_frame
+	queue_free()
+
+func _on_lev_button_down() -> void:
+	$ColorRect/Level.visible = true
+	$ColorRect/Menu.visible = false
+
+func _on_ins_button_down() -> void:
+	$ColorRect/Instructions.visible = true
 	$ColorRect/Menu.visible = false
 
 
-func _on_no_maze_button_down() -> void:
-	Global.ScenetoSpawn = Global.sceneGame
-
-
-func _on_maze_1_button_down() -> void:
-	Global.ScenetoSpawn = Global.maze1
-
-
-func _on_maze_2_button_down() -> void:
-	Global.ScenetoSpawn = Global.maze2
-
-
-func _on_maze_3_button_down() -> void:
-	Global.ScenetoSpawn = Global.maze3
-
-
-func _on_maze_4_button_down() -> void:
-	Global.ScenetoSpawn = Global.maze4
-
-
-func _on_maze_5_button_down() -> void:
-	Global.ScenetoSpawn = Global.maze5
+func _on_maz_button_down() -> void:
+	$ColorRect/Mazes.visible = true
+	$ColorRect/Menu.visible = false
